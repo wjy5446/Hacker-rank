5c6e66a14f689e19de8ce6d8fe8e912f0c41af89
jaeyoungYoon <wjy5446@gmail.com>
2018-01-26 22:59:18 +0900

    Add the source about loops

---
+++ 30-Days-of-Code/Day-5-Loops.py
@@ -0,0 +1,21 @@
+# Objective
+# In this challenge, we're going to use loops to help us do some simple math. Check out the Tutorial tab to learn more.
+#
+# Task
+# Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: n x i = result.
+#
+# Input Format
+#
+# A single integer, .
+#
+# Constraints
+# 
+# Output Format
+#
+# Print  lines of output; each line  (where ) contains the  of  in the form:
+# n x i = result.
+
+n = int(input().strip())
+
+for i in range(1, 11):
+    print("{} x {} = {}".format(n, i, n*i))
