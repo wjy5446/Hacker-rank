dadddff986077cc97ee3405e9bf17ac1c5a075d9
jaeyoungYoon <wjy5446@gmail.com>
2018-02-01 15:05:55 +0900

    Add the contents about abstract classes

---
+++ 30-Days-of-Code/Day-13-AbractClasses.py
@@ -0,0 +1,10 @@
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super(MyBook, self).__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title: {}".format(self.title))
+        print("Author: {}".format(self.author))
+        print("Price: {}".format(self.price))
